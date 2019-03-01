b6be094608fb42d3ff8c3033991f6e7829b477cf
Montgomery <Courtney.Montgomery@capco.com>
2019-03-01 06:21:06 -0600

    added 3 additional charts to display

--- main.js
+++ main.js
@@ -1,5 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
-	let myChart = Highcharts.chart("container", {
+	let chart1 = Highcharts.chart("chart1", {
+		chart: {
+			type: "bar"
+		},
+		title: {
+			text: "My Test Chart"
+		},
+		xAxis: {
+			title: {
+				text: "Fruit eaten"
+			}
+		},
+		series: [
+			{
+				name: "Courtney",
+				data: [1, 0, 9]
+			},
+			{
+				name: "Senia",
+				data: [5, 7, 3]
+			}
+		]
+	});
+
+	let chart2 = Highcharts.chart("chart2", {
+		chart: {
+			type: "bar"
+		},
+		title: {
+			text: "Net Scheduled Interchange - 5 Minute Interval"
+		},
+		xAxis: {
+			title: {
+				text: "Instance"
+			}
+		},
+		series: [
+			{
+				name: "Courtney",
+				data: [1, 0, 9]
+			},
+			{
+				name: "Senia",
+				data: [5, 7, 3]
+			}
+		]
+	});
+
+	let chart3 = Highcharts.chart("chart3", {
+		chart: {
+			type: "bar"
+		},
+		title: {
+			text: "My Test Chart"
+		},
+		xAxis: {
+			title: {
+				text: "Fruit eaten"
+			}
+		},
+		series: [
+			{
+				name: "Courtney",
+				data: [1, 0, 9]
+			},
+			{
+				name: "Senia",
+				data: [5, 7, 3]
+			}
+		]
+	});
+
+	let chart4 = Highcharts.chart("chart4", {
 		chart: {
 			type: "bar"
 		},
@@ -23,18 +95,3 @@ document.addEventListener("DOMContentLoaded", () => {
 		]
 	});
 });
-
-// let chart1;
-// document.addEventListener("DOMContentLoaded", function() {
-// 	chart1 = Highcharts.stockChart("container", {
-// 		rangeSelector: {
-// 			selected: 1
-// 		},
-// 		series: [
-// 			{
-// 				name: "USD to EUR",
-// 				data: usdtoeur
-// 			}
-// 		]
-// 	});
-// });
